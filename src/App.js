c606cedd77f3437de946e99d9b773bc4a9db06bf
robin-england <robin.england94@googlemail.com>
2024-06-29 19:08:33 -0400

    Add personal information section with styled components.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+// U63166005
+// Challenge 15
 
-function App() {
+import React from 'react'
+import PersonalInfo from './PersonalInfo.js'  // imports Profile from component
+import './App.css'    // imports style
+  
+function App() {  // Runs personalinfo
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <PersonalInfo/>
+  )
 }
 
-export default App;
+export default App
---
+++ src/PersonalInfo.js
@@ -0,0 +1,11 @@
+// Create a component to display your name, a brief biography, and contact information such as email and LinkedIn.
+
+export default function PersonalInfo() {
+    return (
+        <div className="personal-info">
+            <h3>Robin England</h3>
+            <p>This is my bio.</p>
+            <p>This is my contact info.</p>
+        </div>
+    )
+}
